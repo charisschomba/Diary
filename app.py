48062d396e7c9a94a7df4ce2b658da96eb067a7b
charisschomba <charisschomba@gmail.com>
2018-07-15 22:48:20 +0300

    [feature #159058819]  implemented creation of entry functionality.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask,jsonify
+from datetime import datetime
+from flask import Flask
+from flask import jsonify
+from flask import request
 
 Entries = []
 
@@ -8,8 +11,23 @@ app = Flask(__name__)
 def get_items():
     response = jsonify({'All Entries':Entries})
     return response
+# This function creates a new entry in the diary
+@app.route('/mydiary/api/v1/entries',methods = ['POST'])
+def create_entry():
+    request_data = request.get_json()
+    if len(Entries) == 0:
+        id = 1
+    else:
+        id = Entries[-1]['id'] + 1  
+    new_entry = {
+    'id': id,
+    'date':datetime.now().strftime ("%d-%m-%Y"),
+    'title':request_data['title'],
+    'content':request_data['content']
+    }
 
-
+    Entries.append(new_entry)
+    return (jsonify(new_entry)),201
 
 
     
