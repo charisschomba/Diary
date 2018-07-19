c710ed72a46d08901c5d69b7c9c987298db693d8
charisschomba <charisschomba@gmail.com>
2018-07-19 17:59:29 +0300

    [Chore #159166075] Updated doc string for models

--- Api/app/Models/entries.py
+++ Api/app/Models/entries.py
@@ -4,36 +4,44 @@ class Entry():
     and also have methods that will manipulate
     those entries.
     """
-# This list stores all entries
+    """This list stores all entries"""
     Entries = []
-# This method fetches entry by its id
+    """This method fetches entry by its id"""
     @classmethod
     def get_by_id(cls,entryId):
         cls.entry = next(filter(lambda x: x['id'] == entryId, cls.Entries), None)
         return cls.entry
-# Appends an entry to the list
+
     def save(self,entry):
+        """Appends an entry to the list"""
         self.Entries.append(entry)
-# deletes an entry
+
     def delete_entry(self,entry):
+        """deletes an entry"""
         self.Entries.remove(entry)
-# updates an entry if it exists with new data
+
     def update_entry(self,new_entry,id_):
+        """updates an entry if it exists with new data"""
         self.entry = next(filter(lambda x: x['id'] == id_, self.Entries), None)
         self.entry.update(new_entry)
-# fetches all entries
+
     def all_items(self):
+        """fetches all entries"""
         return self.Entries
-# returns an object as a string
+
     def __str__(self):
+        """returns an object as a string"""
         return(str(self.Entries))
-# returns the length of an object
+
     def __len__(self):
+        """returns the length of an object"""
         return len(self.Entries)
-# this method allows us to undex an object
+
     def __getitem__(self,i):
+        """this method allows us to undex an object"""
         return self.Entries[i]
-# clears all data stored in Entries
+
     def clear_all(self):
+        """clears all data stored in Entries"""
         data = self.Entries
         del data[:]
