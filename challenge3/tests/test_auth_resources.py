7490209715d451bfb50654fd205ac41c4696f3ea
charisschomba <charisschomba@gmail.com>
2018-07-24 16:54:59 +0300

    Chore-#159265375 Addded Aunthetication test Each test tests it's independent scope.

--- challenge3/tests/test_auth_resources.py
+++ challenge3/tests/test_auth_resources.py
@@ -14,9 +14,14 @@ class Test_Auth_Endpoints(unittest.TestCase):
                 'password':'test',
                 'confirm_password':'test'}
         self.new_user2 = {"username":'test',
+            'email':'test1@camp.com',
+            'password':'test',
+            'confirm_password':'test1'}
+        self.new_user3 = {"username":'test',
             'email':'test@camp.com',
             'password':'test',
             'confirm_password':'test1'}
+
         self.user = {
             'email':'test@camp.com',
             'password':'test',
@@ -34,10 +39,19 @@ class Test_Auth_Endpoints(unittest.TestCase):
         self.assertEquals(response.status_code,201)
         res = json.loads(response.data.decode('utf8'))
         self.assertIn("Account created",str(res))
-        #password don't match
+
+    def test_singup_with_unmatched_password(self):
+#Tests for password mismatch
         response = self.tester.post(self.singup_url,data=json.dumps(self.new_user2),
         content_type="application/json")
         self.assertEquals(response.status_code,400)
+
+# Test sing up with existing email
+    def test_singup_with_existing_email(self):
+        response = self.tester.post(self.singup_url,data=json.dumps(self.new_user),
+        content_type="application/json")
+        self.assertEquals(response.status_code,400)
+
 
 # Test sing_in endpoint
     def test_singin_endpoint(self):
@@ -46,11 +60,20 @@ class Test_Auth_Endpoints(unittest.TestCase):
         self.assertEquals(response.status_code,200)
         res = json.loads(response.data.decode('utf8'))
         self.assertIn("token",str(res))
-        #password don't match
-        response = self.tester.post(self.singup_url,data=json.dumps(self.user2),
+#password don't match
+    def test_sing_with_incorrect_password(self):
+        response = self.tester.post(self.singin_url,data=json.dumps(self.user2),
         content_type="application/json")
-        self.assertEquals(response.status_code,400)
         res = json.loads(response.data.decode('utf8'))
         self.assertIn("password is incorrect",str(res))
+#sign in with  unregisted email
+def test_singin_with_unexisting_email(self):
+        response = self.tester.post(self.singin_url,data=json.dumps(self.user2),
+        content_type="application/json")
+        res = json.loads(response.data.decode('utf8'))
+        self.assertIn("password is incorrect",str(res))
+        res = json.loads(response.data.decode('utf8'))
+        self.assertEquals(response.status_code,400)
+
 
 
