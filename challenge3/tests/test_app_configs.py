6d8adb925a6d98d71a196f318f27b5fd4e7f1d6c
charisschomba <charisschomba@gmail.com>
2018-07-28 20:22:04 +0300

    Chore Added App config tests

---
+++ challenge3/tests/test_app_configs.py
@@ -0,0 +1,29 @@
+import unittest
+from app import create_app
+
+class TestAppConfigs(unittest.TestCase):
+    """
+    This class tests app configuration settings such as Debug and Secret key
+    """
+
+    def test_DefaultConfig(self):
+        app = create_app("production")
+        self.tester = app.test_client(self)
+        self.assertEqual(True,app.config['JSONIFY_PRETTYPRINT_REGULAR'])
+        self.assertTrue('http://127.0.0.1:5000/',app.config["SERVER_NAME"])
+
+    def test_productionConfig(self):
+        app = create_app("production")
+        self.tester = app.test_client(self)
+        self.assertEqual(False,app.config['DEBUG'])
+
+    def test_TestingConfig(self):
+        app = create_app("testing")
+        self.tester = app.test_client(self)
+        self.assertEqual(True,app.config['DEBUG'])
+
+    def test_DevelopmentConfig(self):
+        app = create_app("development")
+        self.tester = app.test_client(self)
+        self.assertEqual(True,app.config['DEBUG'])
+
