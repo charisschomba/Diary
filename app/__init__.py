8f2ff9c9b3d75328f597dcff9a28e4f3a9d09740
charisschomba <charisschomba@gmail.com>
2018-08-12 13:22:08 +0300

    deleted uneccessary files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ This module intializes the app and imports all modules required
 from flask import Flask, render_template
 from flask_restful import Api, Resource
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 from config import app_config
 from app.auth import SingUp, Login
 from app.entries import Entries, EntryList
@@ -15,7 +14,6 @@ def create_app(config_name):
     CORS(app)
     app.config.from_object(app_config[config_name])
     api = Api(app)
-    JWTManager(app)
     api.add_resource(SingUp, '/mydiary/v1/auth/register')
     api.add_resource(Login, '/mydiary/v1/auth/login')
     api.add_resource(Entries, '/mydiary/v1/entries')
